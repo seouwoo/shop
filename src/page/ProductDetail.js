3a4a164c5f8a42da6a7dd1824265ca0a54ce0444
seou <163283988+seouwoo@users.noreply.github.com>
2024-07-09 12:54:59 +0900

    Update ProductDetail.js

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -12,7 +12,7 @@ function ProductDetail() {
   const [product,setProduct]=useState(null);
   //console.log(id)
   const getProductDetail=async()=>{
-    let url=`http://localhost:3004/products/${id}`;
+    let url=`https://my-json-server.typicode.com/seouwoo/shop/products/${id}`;
     let response=await fetch(url);
     let data=await response.json();
     console.log(data)
